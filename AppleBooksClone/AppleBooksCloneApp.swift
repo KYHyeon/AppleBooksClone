6c784b7d93ecbad687807b347ec3eb714b490aa3
KYHyeon <run190822@gmail.com>
2023-01-18 16:12:05 +0900

    Initial Commit

---
+++ AppleBooksClone/AppleBooksCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleBooksCloneApp.swift
+//  AppleBooksClone
+//
+//  Created by 현기엽 on 2023/01/18.
+//
+
+import SwiftUI
+
+@main
+struct AppleBooksCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
