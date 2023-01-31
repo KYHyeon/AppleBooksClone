aafb66d421dc2cc4b840853631c4f460efcbb84d
jamie.kao <jamie.kao@kakaopaycorp.com>
2023-01-31 21:24:27 +0900

    [BookStore] 샘플 화면 스크롤 수정

--- Projects/BookStore/Sources/BookStoreView.swift
+++ Projects/BookStore/Sources/BookStoreView.swift
@@ -4,8 +4,33 @@ public struct BookStoreView: View {
     public init() {}
 
     public var body: some View {
-        Text("BookStore")
-            .padding()
+        NavigationStack {
+            ScrollView {
+                GradationView()
+            }
+            .navigationTitle("북스토어")
+        }
+    }
+}
+
+struct GradationView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("소설 및 문학")
+            ScrollView(.horizontal, showsIndicators: false) {
+                LazyHStack {
+                    ForEach(0..<30) { i in
+                        AsyncImage(
+                            url: URL(string: "https://picsum.photos/seed/books\(i)/150/200"),
+                            content: { $0.resizable() },
+                            placeholder: { ProgressView() }
+                        )
+                        .frame(width: 150, height: 200)
+                    }
+                }
+            }
+            Button("모두 보기") {}
+        }
     }
 }
 
